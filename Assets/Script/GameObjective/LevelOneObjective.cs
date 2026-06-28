c29289c00b3fe16dc51a3bc0e10c477b5235d35f
Oaktheplayer <oaktheplayer@gmail.com>
2026-06-28 11:39:45 +0800

    feat: GameObjective

---
+++ Assets/Script/GameObjective/LevelOneObjective.cs
@@ -0,0 +1,20 @@
+using DoorSystem;
+using UnityEngine;
+using static DoorSystem.DoorControl;
+using static DoorSystem.DoorControl.DoorState;
+
+public class LevelOneObjective : GameObjective
+{
+    public override bool IsComplete()
+    {
+        DoorState notLocked = ~Locked;
+        bool AllDoorsLocked = !DoorManager.Instance.HasDoorWithState(notLocked);
+        return AllDoorsLocked;
+    }
+
+    [ContextMenu("Check Completion Status")]
+    private void CheckComplete()
+    {
+        Debug.Log($"[LevelOneObjective] Checking completion status: {IsComplete()}");
+    }
+}
